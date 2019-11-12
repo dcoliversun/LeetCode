ed434fe55e0e1da067e6b5d70df76554efa5d471
OliverSun <sunqian18s@ict.ac.cn>
2019-11-12 10:53:13 +0800

    Problem 2 Optimization

--- Code/2_Add_Two_Numbers.cpp
+++ Code/2_Add_Two_Numbers.cpp
@@ -9,20 +9,20 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* p = new ListNode(0);
-        ListNode* q = p;
+        ListNode* cur = new ListNode(0);
+        ListNode* dummy = cur;
         int carry = 0;
         while (l1 != NULL || l2 != NULL) {
             int x = (l1 != NULL) ? l1->val : 0;
             int y = (l2 != NULL) ? l2->val : 0;
-            int sum = x + y + carry;
-            carry = sum / 10;
-            p->next = new ListNode(sum % 10);
-            p = p->next;
+            int num = x + y + carry;
+            carry = num / 10;
+            cur->next = new ListNode(num % 10);
+            cur = cur->next;
             if (l1 != NULL) l1 = l1->next;
             if (l2 != NULL) l2 = l2->next;
         }
-        if (carry) p->next = new ListNode(carry);
-        return q->next;
+        if (carry) cur->next = new ListNode(carry);
+        return dummy->next;
     }
 };

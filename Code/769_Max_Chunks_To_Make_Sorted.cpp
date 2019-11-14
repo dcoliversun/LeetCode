8fe6f0057da09569eb60c6274defbd8bc278a2ec
OliverSun <sunqian18s@ict.ac.cn>
2019-11-14 10:56:40 +0800

    Problem 769 Done

---
+++ Code/769_Max_Chunks_To_Make_Sorted.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        stack<int> stk;
+        for (int& num : arr) {
+            if (stk.empty() || num > stk.top()) {
+                stk.push(num);
+            } else {
+                int top = stk.top();
+                stk.pop();
+                while (!stk.empty() && num < stk.top()) stk.pop();
+                stk.push(top);
+            }
+        }
+        return stk.size();
+    }
+};
